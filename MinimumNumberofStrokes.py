5ce6b8f6e9dd9913d987351b150c86192aad79a9
Arshiya Naheed <72481315+arshiya19@users.noreply.github.com>
2024-10-21 00:00:49 -0700

    Completed One problem - 20Oct

--- MinimumNumberofStrokes.py
+++ MinimumNumberofStrokes.py
@@ -1,16 +1,38 @@
+'''
+Given a string where each character (A-Z) represents a paint color, determine the minimum number of strokes required to repaint the string into a uniform color. 
+For example, for the string "ABBAABZBB", the minimum number of strokes required is 4
+'''
+
+''' while the intention was to use a two-pointer technique to determine the minimum strokes needed, the logic implemented only counts character transitions.  '''
+# s = "ABBAABZB"
+
+# st = list(s)
+
+# prev = 0
+# current = 1
+# min_strokes = 1  
+
+# while current < len(st):
+#     if st[prev] != st[current]:  
+#         min_strokes += 1
+#         prev = current  
+
+#     current += 1  
+
+# print(min_strokes)
+
+
+'''To correctly solve the problem of determining how many characters need to be changed to achieve a uniform string, an approach based on counting character frequencies would be more effective.'''
+
+from collections import Counter
+
 s = "ABBAABZB"
+count = Counter(s)
+min_number_of_strokes = 0
 
-st = list(s)
 
-prev = 0
-current = 1
-min_strokes = 1  # Start with 1 stroke for the first character
+max_freq = max(count.values())
 
-while current < len(st):
-    if st[prev] != st[current]:  # Increment stroke count only if a change occurs
-        min_strokes += 1
-        prev = current  # Update prev to current when a change is detected
+min_number_of_strokes = len(s) - max_freq
 
-    current += 1  # Always move current to the next character
-
-print(min_strokes)
+print(min_number_of_strokes)
